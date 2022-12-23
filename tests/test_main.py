13e0baffb0484f18001543fd243ae23a7e71ea3f
Sébastien Eustace <sebastien@eustace.io>
2022-12-23 11:06:41 +0100

    Add support for time and date types in instance()

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,58 @@
 from __future__ import annotations
 
+from datetime import date
+from datetime import datetime
+from datetime import time
+
 import pytz
 
+from dateutil import tz
+
+import pendulum
+
 from pendulum import _safe_timezone
+from pendulum import timezone
 from pendulum.tz.timezone import Timezone
+
+
+def test_instance_with_naive_datetime_defaults_to_utc():
+    now = pendulum.instance(datetime.now())
+    assert now.timezone_name == "UTC"
+
+
+def test_instance_with_aware_datetime():
+    now = pendulum.instance(datetime.now(timezone("Europe/Paris")))
+    assert now.timezone_name == "Europe/Paris"
+
+
+def test_instance_with_aware_datetime_pytz():
+    now = pendulum.instance(datetime.now(pytz.timezone("Europe/Paris")))
+    assert now.timezone_name == "Europe/Paris"
+
+
+def test_instance_with_aware_datetime_any_tzinfo():
+    dt = datetime(2016, 8, 7, 12, 34, 56, tzinfo=tz.gettz("Europe/Paris"))
+    now = pendulum.instance(dt)
+    assert now.timezone_name == "+02:00"
+
+
+def test_instance_with_date():
+    dt = pendulum.instance(date(2022, 12, 23))
+
+    assert isinstance(dt, pendulum.Date)
+
+
+def test_instance_with_naive_time():
+    dt = pendulum.instance(time(12, 34, 56, 123456))
+
+    assert isinstance(dt, pendulum.Time)
+
+
+def test_instance_with_aware_time():
+    dt = pendulum.instance(time(12, 34, 56, 123456, tzinfo=timezone("Europe/Paris")))
+
+    assert isinstance(dt, pendulum.Time)
+    assert dt.tzinfo.name == "Europe/Paris"
 
 
 def test_safe_timezone_with_tzinfo_objects():
